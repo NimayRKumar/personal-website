e9ad3dfbaab05b59f278f066767699283826b250
NimayRKumar <kumar.nimay@yahoo.com>
2021-09-28 09:30:06 -0500

    back to react-bootstra

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
 import React, { Component } from 'react';
-import { slide as Menu } from 'react-burger-menu'
+import { Tabs, Tab, Container, Nav, Navbar } from 'react-bootstrap'
 import './App.css';
-import Sidebar from './components/Sidebar'
-import Home from './components/Home'
-import Intro from './components/Intro'
+import 'bootstrap/dist/css/bootstrap.css';
 
-var styles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '36px',
-    top: '36px'
-  },
-  bmBurgerBars: {
-    background: '#373a47'
-  },
-  bmBurgerBarsHover: {
-    background: '#a90000'
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px'
-  },
-  bmCross: {
-    background: '#bdc3c7'
-  },
-  bmMenuWrap: {
-    position: 'fixed',
-    height: '100%'
-  },
-  bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.75em'
-  },
-  bmMorphShape: {
-    fill: '#373a47'
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em'
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)'
-  }
-}
+import About from './components/About'
 
 class App extends Component {
 
   render() {
     return (
 
-      <Menu styles={styles}>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="projects" className="menu-item" href="/projects">Projects</a>
-        <a id="music" className="menu-item" href="/Music">Music</a>
-      </Menu>
+      <Navbar bg="dark" variant="dark">
+          <Container>
+              <Nav>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#music">Music</Nav.Link>
+              </Nav>
+          </Container>
+      </Navbar>
+ 
     )
   }
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import { React } from 'react'
 
-const Intro = () => {
+const About = () => {
 
     const whoAmI = "I'm Nimay! I'm a software engineer who studied Electrical & Computer Engineering at the University of Texas at Austin!"
 
@@ -10,4 +10,4 @@ const Intro = () => {
 
 }
 
-export default Intro
+export default About
