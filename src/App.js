b5240a88b6a2fe34586813463e0bbd56180228f3
NimayRKumar <kumar.nimay@yahoo.com>
2021-10-03 09:40:03 -0500

    under construction

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import { Tabs, Tab, Container, Nav, Navbar } from 'react-bootstrap'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import About from './components/About'
-
 class App extends Component {
 
   render() {
     return (
 
-      <Navbar bg="dark" variant="dark">
-          <Container>
-              <Nav>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#music">Music</Nav.Link>
-              </Nav>
-          </Container>
-      </Navbar>
+      <div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+
+              
+                <Nav>
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#about">About</Nav.Link>
+                      <Nav.Link href="#projects">Projects</Nav.Link>
+                      <Nav.Link href="#music">Music</Nav.Link>
+
+                </Nav>
+
+                
+            </Container>
+        </Navbar>
+
+        <p>Under construction...</p>
+
+      </div>
  
     )
   }
---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import { React } from 'react'
+
+const Intro = () => {
+
+    const whoAmI = "I'm Nimay! I'm a software engineer who studied Electrical & Computer Engineering at the University of Texas at Austin!"
+
+    return (
+        <p>{whoAmI}</p>
+    )
+
+}
+
+export default Intro
