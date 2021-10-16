5aa0ed37883651ab13d5f72b8763d2b9a675b258
NimayRKumar <kumar.nimay@yahoo.com>
2021-10-16 14:55:07 -0500

    Navbar redesign

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Container, Nav, Navbar } from 'react-bootstrap'
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
 
   render() {
     return (
-
-      <div>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-
-              
-                <Nav>
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#about">About</Nav.Link>
-                      <Nav.Link href="#projects">Projects</Nav.Link>
-                      <Nav.Link href="#music">Music</Nav.Link>
-
-                </Nav>
-
-                
-            </Container>
-        </Navbar>
-
-        <p>Under construction...</p>
-
+      <div className="App">
+        <Navbar/>
       </div>
  
     )
--- src/components/Sidebar.js
+++
@@ -1,9 +0,0 @@
-
-const Sidebar = () => {
-
-    return (
-        <p>SIDE BAR</p>
-    )
-}
-
-export default Sidebar
