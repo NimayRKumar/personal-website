38f92d3d69cab002c3fb972b07e2f585ed07c4a6
NimayRKumar <kumar.nimay@yahoo.com>
2022-10-17 11:32:56 -0400

    bugspray

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import './App.css';
 import About from './components/About/About'
-import Music from './components/Music/Music'
-import Outdoors from './components/Outdoors/Outdoors'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -13,12 +11,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          {
-          //<Navbar/>
-          }
           <Route exact path='/' component={About}/>
-          <Route exact path='/music' component={Music}/>
-          <Route exact path='/outdoors' component={Outdoors}/>
         </div>
       </Router>
     )
