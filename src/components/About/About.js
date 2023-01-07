26a59fc866f6fe13b9ce7944e71c8f7433279872
NimayRKumar <kumar.nimay@yahoo.com>
2023-01-07 15:47:07 -0500

    added gpa. thanks hassani

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -72,7 +72,7 @@ const About = () => {
                 <div className={styles.resume}>
                     <p>
                         <a 
-                            href="https://drive.google.com/file/d/1RzIWgZfowuqn1c21jYRbXoS3ZexZhwPZ/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1dPr1Hj8CNfP300VO97ZXWdyCaCipw897/view?usp=sharing"
                             target="_blank"
                         >
                             Resume
